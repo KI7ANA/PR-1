0e0a4631a32d638e2d3a336fcaf79032bd31f80c
KI7ANA <sergej.tkachev.02@bk.ru>
2026-04-27 09:13:49 +0900

    Add Android project source files for PR1

---
+++ app/src/main/java/com/ncfu/pw_1/MainActivity.java
@@ -0,0 +1,12 @@
+package com.ncfu.pw_1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_variant5);
+    }
+}
