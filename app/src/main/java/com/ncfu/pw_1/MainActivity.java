0de15e08082065e12af9dbe68266c27096b544e7
KI7ANA <sergej.tkachev.02@bk.ru>
2026-04-27 09:31:13 +0900

    Update MainActivity in PR1

--- app/src/main/java/com/ncfu/pw_1/MainActivity.java
+++ app/src/main/java/com/ncfu/pw_1/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Основная activity первой практической работы
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
